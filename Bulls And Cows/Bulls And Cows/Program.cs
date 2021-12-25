41173861370c5892c4617a5295fd6a791af271b5
Yevgeni Karpilovsky <65335246+jk21-HH@users.noreply.github.com>
2021-12-25 15:45:06 +0200

    Add files via upload

---
+++ Bulls And Cows/Bulls And Cows/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+
+namespace Bulls_And_Cows
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            bool isPlay = true;
+            while (isPlay)
+            {
+                Console.Clear();
+                Game currentGame = new Game(UserInterface.GetNumberOfGuesses());
+                currentGame.PlayGame(out isPlay);
+                if (!isPlay)
+                {
+                    Thread.Sleep(2000);
+                    break;
+                }
+                else
+                {
+                    isPlay = UserInterface.IsNewGame();
+                }
+            }
+        }
+    }
+}
